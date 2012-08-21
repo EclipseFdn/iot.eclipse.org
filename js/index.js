9b51d752f2f885be90271cfc7f3622d28a35f65d
Benjamin Cabé <BCabe@sierrawireless.com>
2012-08-21 14:56:45 +0200

    Initial check-in.

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+/**
+ * Onload handler
+ */
+$(document).ready(function(){
+    //Enable dropdowns
+    $('.dropdown-toggle').dropdown();
+
+    //Start carousel
+    $('.carousel').carousel({
+	  interval: 4000,
+	  pause: 'hover'
+	});
+});
