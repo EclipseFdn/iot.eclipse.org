5f423f53ba0037539d4c5e0ed73072c2882904f9
Benjamin Cabé <BCabe@sierrawireless.com>
2013-11-18 10:48:44 +0100

    misc look&feel and spelling fixes added commented out carousel elements

--- js/index.js
+++ js/index.js
@@ -1,9 +1,26 @@
+$.fn.shuffle = function() {
+    return this.each(function(){
+        var items = $(this).children().clone(true);
+        return (items.length) ? $(this).html($.shuffle(items)) : this;
+    });
+}
+
+$.shuffle = function(arr) {
+    for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+    return arr;
+}
+
 /**
  * Onload handler
  */
 $(document).ready(function() {
 	// Enable dropdowns
 	$('.dropdown-toggle').dropdown();
+
+	// Shuffle carousel items
+    $('.carousel-inner').shuffle();
+
+    $('.carousel-inner').children()[0].className+=" active";
 
 	// Start carousel
 	$('.carousel').carousel({
