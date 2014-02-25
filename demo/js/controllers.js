f6e3b746a8107b3f028c2b30e001c4c4c492f836
Benjamin Cabé <BCabe@sierrawireless.com>
2014-02-25 19:32:36 +0100

    wip fixing greenhouse demo

--- demo/js/controllers.js
+++ demo/js/controllers.js
@@ -12,7 +12,35 @@ config) {
 	$scope.luminosity = {value: 0.0, timestamp: dateFormat(new Date())}
 	$scope.humidity = {value: 0.0, timestamp: dateFormat(new Date())}
 	$scope.webcam = {src: "http://91.121.117.128/webcam/?action=stream"}
-	$scope.roof = {value: "OFF", img: "roof_OPEN.svg", timestamp: dateFormat(new Date())}
+	$scope.roof = {value: "TOGGLE", img: "roof_OPEN.svg", timestamp: dateFormat(new Date())}
+
+	var client = new Messaging.Client("ws://iot.eclipse.org/ws", "clientId");
+	client.onConnectionLost = onConnectionLost;
+	client.onMessageArrived = onMessageArrived;
+	client.connect({onSuccess:onConnect});
+
+	function onConnect() {
+	  // Once a connection has been made, make a subscription and send a message.
+	  console.log("onConnect");
+	  client.subscribe("/fosdem/#");
+	};
+	function onConnectionLost(responseObject) {
+	  if (responseObject.errorCode !== 0)
+	    console.log("onConnectionLost:"+responseObject.errorMessage);
+	};
+	function onMessageArrived(message) {
+	  var topic = message.destinationName;
+	  var sensor = topic.substring(topic.lastIndexOf("/") + 1, topic.length);
+	  console.log("SENSOR: " + sensor);
+	  if($scope[sensor]) {
+	    $scope[sensor].value = message.payloadString;
+	    $scope[sensor].timestamp = dateFormat(new Date());
+	    $scope.$apply();
+	  }
+	  //console.log("onMessageArrived: "+message.destinationName +": " +message.payloadString);
+	};	
+
+
 
 	function tick (){
 		restService.getDeviceData($routeParams.deviceId,
@@ -39,21 +67,13 @@ config) {
 
 		// $scope.webcam.src = "http://www.benjamin-cabe.com/webcam.jpg?" + new Date().getTime();
 
-		$timeout(tick, 1000);
+//		$timeout(tick, 1000);
 	}
-	tick();
+//	tick();
 
 	$scope.toggleRoof = function() {
-
-
-		var postData = {
-			"settings": [{
-				"key": "greenhouse.commands.data.toggleRoof.state",
-				"value": !this.roof.state
-			}]
-		};
-
-		restService.sendData($routeParams.deviceId, postData, function(data){console.log(data)}, function(status){console.log(status)});
-
+	  var message = new Messaging.Message("TOGGLE");
+	  message.destinationName = "/fosdem/demo/command/toggleRoof";
+	  client.send(message); 
 	}
 }
