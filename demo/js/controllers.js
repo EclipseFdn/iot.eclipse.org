d9f1c34318a436fb1746426f987bcc8c57e0e483
Benjamin Cabé <BCabe@sierrawireless.com>
2014-03-02 13:10:32 +0100

    webcam

--- demo/js/controllers.js
+++ demo/js/controllers.js
@@ -31,7 +31,7 @@ config) {
 	function onMessageArrived(message) {
 	  var topic = message.destinationName;
 	  var sensor = topic.substring(topic.lastIndexOf("/") + 1, topic.length);
-	  console.log("SENSOR: " + sensor);
+	  //console.log("SENSOR: " + sensor);
 	  if($scope[sensor]) {
 	    $scope[sensor].value = message.payloadString;
 	    $scope[sensor].timestamp = dateFormat(new Date());
@@ -40,38 +40,14 @@ config) {
 	  //console.log("onMessageArrived: "+message.destinationName +": " +message.payloadString);
 	};	
 
-
-
 	function tick (){
-		restService.getDeviceData($routeParams.deviceId,
-
-			function(data){
-					      $scope.temperature.value = parseFloat(data.temperature.value).toFixed(1);
-					      $scope.temperature.timestamp = dateFormat(new Date(parseInt(data.temperature.timestamp)));
-
-					      $scope.luminosity.value = parseFloat(data.luminosity.value).toFixed(1);
-					      $scope.luminosity.timestamp = dateFormat(new Date(parseInt(data.luminosity.timestamp)));
-
-					      $scope.humidity.value = parseFloat(data.humidity.value).toFixed(1);
-					      $scope.humidity.timestamp = dateFormat(new Date(parseInt(data.humidity.timestamp)));
-
-					          var roof = data.open 
-						  roof.img = 'roof_' + roof.value + '.svg' 
-						  roof.state = (roof.value == "CLOSE")
-						  $scope.roof = roof
-						      $scope.roof.timestamp = dateFormat(new Date(parseInt(data.open.timestamp)));
-
-			}, 
-
-			function(status){console.log(status)});
-
-		// $scope.webcam.src = "http://www.benjamin-cabe.com/webcam.jpg?" + new Date().getTime();
-
-//		$timeout(tick, 1000);
+		$scope.webcam.src = "http://www.benjamin-cabe.com/webcam.jpg#" + new Date().getTime();
+		$timeout(tick, 1000);
 	}
-//	tick();
+	tick();
 
 	$scope.toggleRoof = function() {
+		console.log('toggle');
 	  var message = new Messaging.Message("TOGGLE");
 	  message.destinationName = "/fosdem/demo/command/toggleRoof";
 	  client.send(message); 
