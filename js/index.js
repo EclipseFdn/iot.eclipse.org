6bc2579d02d082ac6bd7c6bc28a0c2ea93667158
Benjamin CABE <benjamin@eclipse.org>
2014-04-08 10:57:00 +0200

    shuffle members on the ecosystem page add Eurotech

--- js/index.js
+++ js/index.js
@@ -1,45 +1,36 @@
-$.fn.shuffle = function() {
-    return this.each(function(){
-        var items = $(this).children().clone(true);
-        return (items.length) ? $(this).html($.shuffle(items)) : this;
-    });
-}
-
-$.shuffle = function(arr) {
-    for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
-    return arr;
-}
-
 /**
  * Onload handler
  */
 $(document).ready(function() {
-	// Enable dropdowns
-	$('.dropdown-toggle').dropdown();
+    // Enable dropdowns
+    $('.dropdown-toggle').dropdown();
 
-	// Shuffle carousel items
+    // Shuffle carousel items
     $('.carousel-inner').shuffle();
 
-    $('.carousel-inner').children()[0].className+=" active";
+    $('.carousel-inner').children()[0].className += " active";
 
-	// Start carousel
-	$('.carousel').carousel({
-		interval : 4000,
-		pause : 'hover'
-	});
+    // Start carousel
+    $('.carousel').carousel({
+        interval: 4000,
+        pause: 'hover'
+    });
 
-	$('#iot-definition').popover({placement:'bottom', trigger:'hover'});
+    $('#iot-definition').popover({
+        placement: 'bottom',
+        trigger: 'hover'
+    });
 
 });
 
 $(window).bind("load", function() {
-	$('.home-box').addClass('loaded');
+    $('.home-box').addClass('loaded');
 });
 
 
 ($("a[data-toggle=modal]")).click(function() {
-	  var target, url;
-	  target = ($(this)).attr('data-target');
-	  url = ($(this)).attr('href');
-	  return ($(target)).load(url);
-	});
+    var target, url;
+    target = ($(this)).attr('data-target');
+    url = ($(this)).attr('href');
+    return ($(target)).load(url);
+});
