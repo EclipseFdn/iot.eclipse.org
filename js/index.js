5eb23d949db9da8fe81a639c7300c4afb4df88ed
Benjamin Cabé <benjamin@eclipse.org>
2015-01-08 12:03:39 +0100

    new prizes

--- js/index.js
+++ js/index.js
@@ -5,9 +5,21 @@ $(document).ready(function() {
     // Enable dropdowns
     $('.dropdown-toggle').dropdown();
 
-    $('#iot-definition').popover({
+    $('a[rel=popover]').popover({
+        html: true,
         placement: 'bottom',
         trigger: 'hover'
+    });
+
+    $('.img-microej').popover({
+        html: true,
+        placement: 'bottom',
+        trigger: 'hover',
+        template: '<div class="popover" role="tooltip" style="width: 500px;"><div class="arrow"></div><div class="popover-content"><div class="data-content"></div></div></div>',
+        content: function() {
+            return '<img src="/open-iot-challenge/images/microej_IoT_Eclipse_Contest2015.png" />';
+        }
+
     });
 
 });
