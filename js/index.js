2b00e69fef5c13e890348cf081cead4b35116c23
Benjamin Cabé <benjamin@eclipse.org>
2015-01-08 12:06:42 +0100

    jpg

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
         trigger: 'hover',
         template: '<div class="popover" role="tooltip" style="width: 500px;"><div class="arrow"></div><div class="popover-content"><div class="data-content"></div></div></div>',
         content: function() {
-            return '<img src="/open-iot-challenge/images/microej_IoT_Eclipse_Contest2015.png" />';
+            return '<img src="/open-iot-challenge/images/microej_IoT_Eclipse_Contest2015.jpg" />';
         }
 
     });
