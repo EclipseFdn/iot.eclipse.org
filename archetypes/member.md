e936617d587cdb505280b3f54519db0d92c3bd30
Benjamin Cabé <kartben@gmail.com>
2015-03-09 16:08:13 -0700

    add members in Hugo

---
+++ archetypes/member.md
@@ -0,0 +1,3 @@
++++
+demo_url = "http://dummy"
++++
--- public/js/index.js
+++ public/js/index.js
@@ -1,56 +1,11 @@
-/**
- * Onload handler
- */
-$(document).ready(function() {
-    // Enable dropdowns
-    $('.dropdown-toggle').dropdown();
+(function($, window, document) {
+        $(function() {
+                $('.logos .row').shuffle();
 
-    $('a[rel=popover]').popover({
-        html: true,
-        placement: 'bottom',
-        trigger: 'hover'
-    });
+                $('.logos .row').children().filter(function(index) {
+                    return index < 9;
+                }).css( "display", "inline-block");
+        });
 
-    $('.img-microej').popover({
-        html: true,
-        placement: 'bottom',
-        trigger: 'hover',
-        template: '<div class="popover" role="tooltip" style="width: 500px;"><div class="arrow"></div><div class="popover-content"><div class="data-content"></div></div></div>',
-        content: function() {
-            return '<img src="/open-iot-challenge/images/microej_IoT_Eclipse_Contest2015.jpg" />';
-        }
-
-    });
-
-    // var FEED_URL = "https://pipes.yahoo.com/pipes/pipe.run?_id=aedbf3bc8ceeda32ebc49a51a40be9d5&_render=rss";
-    // $.ajax({
-    //     url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(FEED_URL),
-    //     dataType: 'json',
-    //     success: function(data) {
-    //         if (data.responseData.feed && data.responseData.feed.entries) {
-    //             $.each(data.responseData.feed.entries, function(i, e) {
-    //                 console.log("------------------------");
-    //                 console.log(e);
-    //                 console.log("title      : " + e.title);
-    //                 console.log("author     : " + e.author);
-    //                 console.log("description: " + e.description);
-    //             });
-    //         }
-    //     }
-    // });
-
-
-
-});
-
-$(window).bind("load", function() {
-    $('.home-box').addClass('loaded');
-});
-
-
-($("a[data-toggle=modal]")).click(function() {
-    var target, url;
-    target = ($(this)).attr('data-target');
-    url = ($(this)).attr('href');
-    return ($(target)).load(url);
-});
+}(window.jQuery, window, document));
+// The global jQuery object is passed as a parameter
