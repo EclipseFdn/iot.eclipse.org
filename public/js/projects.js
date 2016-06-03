32627e9b01452c7941d6fbef920b9711a0fe7788
Benjamin Cabé <benjamin@eclipse.org>
2016-06-03 09:43:21 +0200

    new project/download page goes live
    Signed-off-by: Benjamin Cabé <benjamin@eclipse.org>

--- public/js/projects.js
+++ public/js/projects.js
@@ -6,26 +6,101 @@
 
 
 var projectsAssociation = {
-    "Standards": ["iot.paho", "iot.paho.incubator", "iot.californium", "iot.om2m", "iot.mosquitto", "iot.moquette", "iot.wakaama", "iot.leshan", "iot.concierge", "iot.risev2g", "iot.4diac", "iot.tiaki", "iot.tinydtls", "iot.tinydtls", "iot.milo"],
+    'Standards': {
+        'iot.paho': true,
+        'iot.paho.incubator': true,
+        'iot.californium': true,
+        'iot.om2m': true,
+        'iot.mosquitto': true,
+        'iot.moquette': true,
+        'iot.wakaama': true,
+        'iot.leshan': true,
+        'iot.concierge': true,
+        'iot.risev2g': true,
+        'iot.4diac': true,
+        'iot.tiaki': true,
+        'iot.tinydtls': true,
+        'iot.milo': true
+    },
 
-    "Frameworks": ["iot.hono", "iot.om2m", "iot.krikkit", "iot.kura", "iot.mihini", "iot.ponte", "iot.smarthome", "iot.eclipsescada", "iot.4diac", "iot.whiskers"],
+    'Frameworks': {
+        'iot.hono': true,
+        'iot.om2m': true,
+        'iot.krikkit': true,
+        'iot.kura': true,
+        'iot.mihini': true,
+        'iot.ponte': true,
+        'iot.smarthome': true,
+        'iot.eclipsescada': true,
+        'iot.4diac': true,
+        'iot.whiskers': true
+    }
 
-    // rest is "others" (Ignite, ...)
+    // rest is 'others' (Ignite, ...)
 };
 
 
 var projectAliases = {
-    "iot.californium": "Californium",
-    "iot.4diac": "4DIAC"
+    'iot.californium': 'Californium',
+    'iot.4diac': '4DIAC'
 };
+
+var releases = {
+  "iot.paho": "1.1.0",
+  "iot.eclipsescada": "0.3.0",
+  "iot.concierge": "5.0",
+  "iot.mosquitto": "1.4",
+  "iot.smarthome": "0.7.0",
+  "iot.kura": "2.0.0",
+  "iot.om2m": "0.8.0",
+  "iot.californium": "1.0.0"
+};
+var download_urls = {
+  "iot.paho": "http://www.eclipse.org/paho/#downloads",
+  "iot.eclipsescada": "http://download.eclipse.org/eclipsescada/",
+  "iot.ponte": "http://eclipse.org/ponte",
+  "iot.concierge": "http://projects.eclipse.org/projects/iot.concierge/downloads",
+  "iot.mosquitto": "https://eclipse.org/mosquitto",
+  "iot.smarthome": "https://www.eclipse.org/smarthome/documentation/community/downloads.html",
+  "iot.kura": "https://www.eclipse.org/kura/downloads.php",
+  "iot.krikkit": "http://projects.eclipse.org/projects/iot.krikkit",
+  "iot.om2m": "https://wiki.eclipse.org/OM2M/Download",
+  "iot.californium": "http://projects.eclipse.org/projects/iot.californium/downloads",
+  "iot.wakaama": "http://projects.eclipse.org/projects/iot.wakaama",
+  "iot.paho.incubator": "http://projects.eclipse.org/projects/iot.paho.incubator",
+  "iot.vorto": "https://www.eclipse.org/vorto/downloads/index.html",
+  "iot.leshan": "https://github.com/eclipse/leshan#test-leshan-locally",
+  "iot.ignite": "http://projects.eclipse.org/projects/iot.ignite",
+  "iot.4diac": "http://www.eclipse.org/4diac",
+  "iot.risev2g": "https://github.com/eclipse/risev2g",
+  "iot.tinydtls": "http://projects.eclipse.org/projects/iot.tinydtls",
+  "iot.tiaki": "http://projects.eclipse.org/projects/iot.tiaki/downloads",
+  "iot.hawkbit": "http://projects.eclipse.org/projects/iot.hawkbit",
+  "iot.hono": "http://projects.eclipse.org/projects/iot.hono",
+  "iot.edje": "http://projects.eclipse.org/projects/iot.edje",
+  "iot.whiskers": "http://projects.eclipse.org/projects/iot.whiskers",
+  "iot.milo": "http://projects.eclipse.org/projects/iot.milo"
+};
+var download_stats = {
+  "iot.leshan": 1294,
+  "iot.californium": 2624,
+  "iot.paho": 20276,
+  "iot.om2m": 138,
+  "iot.mosquitto": 11039,
+  "iot.smarthome": 4034,
+  "iot.eclipsescada": 1091,
+  "iot.kura": 1035,
+  "iot.ponte": 407
+};
+
 
 
 (function($, window, document) {
     $(function() {
 
-        $(window).resize(function() {
-            resize();
-        });
+        // $(window).resize(function() {
+        //     resize();
+        // });
 
         // $(document).on('click', '.itembox, .news-list li', function() {
         //     var link = $(this).find('.readmore').attr('href');
@@ -35,14 +110,15 @@ var projectAliases = {
 
         $.ajax({
             type: "GET",
-//            url: "//projects.eclipse.org/jsonp/technology/Internet%20of%20Things",
-//            dataType: "jsonp",
+            //            url: "//projects.eclipse.org/jsonp/technology/Internet%20of%20Things",
+            //            dataType: "jsonp",
             url: "/projects.json",
             dataType: "json",
             cache: true,
             success: function(data) {
 
                 var projectBoxes = {};
+                var projectInfos = [];
 
                 var val = 20;
                 $('.progress-bar').css('width', val + '%').attr('aria-valuenow', val);
@@ -63,112 +139,112 @@ var projectAliases = {
 
                     var title = stringJanitor(value.name);
                     var id = stringJanitor(value.id);
-                    var desc = stringJanitor(value.description, {
-                        "cut": true,
-                        "ellipsis": "&hellip;"
-                    });
                     var link = value.website;
-                    var logo = value.logo;
-                    var style = "";
-                    var showlogo = false;
-
-                    if (id == 'rt.ecf' || id == 'tools.sequoyah.mtj' || id == "technology.koneki")
-                        return true;
-
-                    var boxOutput = "";
-
-                    boxOutput += "<div class=\"col-md-4 col-sm-6 itembox\"" + style + " id =\"" + id.replace('.', '-') + "\">";
-                    if (validateUrl(logo) && showlogo === true) {
-                        boxOutput += "<img class =\"logo\" alt=\"" + title + " logo\" src=\"" + logo + "\">";
-                    } else {
-                        boxOutput += "<h3 class=\"purple\">" + title + "</h3>";
-                    }
-
-                    boxOutput += "<p>" + desc + "</p>";
-
                     if (!validateUrl(link)) {
                         link = "http://projects.eclipse.org/projects/" + id;
                     }
+                    var desc = stringJanitor(value.description, {
+                        "cut": true,
+                        "ellipsis": ' [&hellip;] <br><a href="' + link + '"> Read more&hellip;</a>'
+                    });
 
+                    var logo = value.logo;
+                    var style = "";
+                    var showlogo = true;
 
-                    boxOutput += "<a href=\"" + link + "\" class=\"readmore\" target=\"_blank\">Read more</i></a>";
-                    boxOutput += "<a href=\"https://projects.eclipse.org/projects/" + id + "/downloads\" class=\"download\" target=\"_blank\">Download</a>";
+                    if (id == 'rt.ecf' || id == 'tools.mtj' || id == "technology.koneki" || id == "tools.titan")
+                        return true;
 
-                    boxOutput += "</div>";
+                    projectInfo = {};
+                    projectInfo.link = value.website;
+                    projectInfo.logo = value.logo || 'http://fakeimg.pl/400x200/f5f5f5/000/?text=' + title //; || 'https://placeholdit.imgix.net/~text?txtsize=42&txt=' + title + '&w=200&h=80&bg=f5f5f5&txtclr=000000' ; //|| 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+                    projectInfo.id = id;
+                    projectInfo.name = title;
+                    projectInfo.labels = '';
+                    for (var k in projectsAssociation) {
+                        if (projectsAssociation[k][value.id]) {
+                            projectInfo.labels += ' ' + ' <span class="label label-warning">' + k + '</span>'
+                        }
+                    }
 
-                    projectBoxes[id] = boxOutput;
+                    projectInfo.description = desc;
+                    projectInfo.fullDescription = stringJanitor(value.description);
+                    var downloads = download_stats[value.id] || 0;
+                    projectInfo.downloads = downloads;
+                    projectInfo.downloadsHuman = (downloads == 0) ? 'N/A' : numeral(downloads).format('0.[0] a');
+                    projectInfo.version = releases[value.id] || 'none';
+                    projectInfo.downloadUrl = download_urls[value.id];
+
+                    projectInfos.push(projectInfo);
 
                     i++;
                 });
 
+
+                // Insert html and resize the boxes.
+                //$("#update-project").append('</div>' + output);
+                //resize();
+
+                var options = {
+                    item: '<li class="col-md-12"><div class="media">\
+                              <div class="media-left media-middle">\
+                                <a href="#" class="link">\
+                                  <img class="media-object img-responsive logo img-thumbnailXXX">\
+                                </a>\
+                              </div>\
+                              <div class="media-body">\
+                                 <h4 class="media-heading name"> </h4><span class="labels"></span>\
+                                   <div class="row">\
+                                    <p class="fullDescription" style="display:none;"></p>\
+                                    <p class="description col-md-8"></p>\
+                                    <div class="col-md-4 details">\
+                                        <p class="downloads" style="display:none;"></p>\
+                                        <div class="col-md-12">Monthly downloads: <span class="badge downloadsHuman">50K+</span></a></div>\
+                                        <div class="col-md-12">Latest release: <span class="badge version">1.4</span></a></div>\
+                                        <div class="col-md-12"><a class="btn btn-sm btn-info downloadUrl" href="#">Download</a></div>\
+                                    </div>\
+                                </div>\
+                              </div>\
+                            </div><hr></li>',
+                    valueNames: ['name', 'description', 'fullDescription', {
+                            name: 'logo',
+                            attr: 'src'
+                        }, 'downloads', 'downloadsHuman', 'version', {
+                            name: 'link',
+                            attr: 'href'
+                        }, {
+                            name: 'downloadUrl', attr:'href'
+                        },
+                        //'labels'
+                    ]
+                };
+
+                var list = new List('project-list', options, projectInfos);
+
+                list.sort('downloads', {
+                    order: "desc"
+                });
+
+                for (var i in list.visibleItems) {
+                    var elem = list.visibleItems[i].elm;
+                    var logoElem = $("img.logo", elem);
+
+                    if (logoElem.attr('src').includes('fakeimg')) {
+                        logoElem.addClass('has-placeholder-logo');
+                    }
+                }
+
                 $("#update-project").empty();
                 $("#update-project").removeClass("loading");
 
-                var output = "";
-
-                for (var k in projectsAssociation) {
-                    output += "<div class=\"container\">";
-                    output += "<h1 class=\"purple\">" + k + "</h1>";
-                    output += "<div class=\"projects-row row-fluid\">";
-
-                    projectsAssociation[k].forEach(function(elem) {
-                        output += projectBoxes[elem];
-                    });
-
-                    output += "</div>"; // end row-fluid
-                    output += "</div>"; // end container
-                }
-
-                // and now ... "The Others"!
-                output += "<div class=\"container\">";
-                output += "<h1 class=\"purple\">Others</h1>";
-                output += "<div class=\"projects-row row-fluid\">";
-
-                for (var k in projectBoxes) {
-                    var found = false;
-                    // is the project in any other category?
-                    for (var kk in projectsAssociation) {
-                        projectsAssociation[kk].forEach(function(elem) {
-                            if (elem == k) {
-                                found = true;
-                                return;
-                            }
-                        });
-                    }
-
-                    if (!found) {
-                        console.log(k);
-                        output += projectBoxes[k];
-                    }
-
-                }
-
-                output += "</div>"; // end row-fluid
-                output += "</div>"; // end container
-
-
-                // Insert html and resize the boxes.
-                $("#update-project").append('</div>' + output);
-                resize();
-
-        //      $('.projects-row').shuffle();
-
-      $('.projects-row').each ( function (index)  {
-	      $(this).find('.itembox').sort(function (a, b) {
-	           // console.log(a.getAttribute('id').toLowerCase());
-	           var s1 = a.getAttribute('id').toLowerCase();
-	           var s2 = b.getAttribute('id').toLowerCase();
-
-	          return s1.localeCompare(s2);
-	      }).replaceAll( $(this) );
-      }   );
-
+                //    setTimeout(1000, rez;
 
 
             },
         });
 
     });
+
     // Validate URL.
     var validateUrl = function validateUrl(str) {
         return (/^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i).test(str);
@@ -176,17 +252,17 @@ var projectAliases = {
 
     // Set same height to all .itembox in a row.
     var resize = function() {
-        $('.itembox').css({
+        $('li').css({
             "height": "auto"
         });
-        $('.row-fluid').each(function() {
+        $('ul').each(function() {
             var highestBox = 0;
-            $('.itembox', this).each(function() {
+            $('li', this).each(function() {
                 if ($(this).height() > highestBox) {
                     highestBox = $(this).outerHeight();
                 }
             });
-            $('.itembox', this).height(highestBox);
+            $('li', this).height(highestBox);
         });
     };
 
@@ -195,7 +271,7 @@ var projectAliases = {
         var settings = $.extend({
             // These are the defaults.
             start: 0,
-            end: 200,
+            end: 250,
             html: false,
             ellipsis: "",
             cut: false,
