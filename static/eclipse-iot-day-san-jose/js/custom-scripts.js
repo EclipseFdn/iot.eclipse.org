2cd45c58fc4b4deb49a61c0a4ca6ee72c5bc223f
Benjamin Cabé <benjamin@eclipse.org>
2016-11-30 08:03:29 +0100

    fix https
    Signed-off-by: Benjamin Cabé <benjamin@eclipse.org>

--- static/eclipse-iot-day-san-jose/js/custom-scripts.js
+++ static/eclipse-iot-day-san-jose/js/custom-scripts.js
@@ -316,7 +316,7 @@
     /*   GOOGLE MAP                                           */
     /**********************************************************/
     function init_map() {
-        var myLocation = new google.maps.LatLng(24.892467,91.87048);
+        var myLocation = new google.maps.LatLng(37.323832038, -121.88666312);
             
             var draggableValue;
             if($(document).width() <= 768){
