c9a2b94012f15a00b900be4c044ac8fee818e850
Eric Poirier <eric.poirier@eclipse-foundation.org>
2018-07-19 15:28:18 -0400

    Infra 2894 - Update footer for GDPR compliance
    Change-Id: I331128607c8c1837cf289c89358ec04fd6b33f6c
    Signed-off-by: Eric Poirier <eric.poirier@eclipse-foundation.org>

--- static/gruntfile.js
+++ static/gruntfile.js
@@ -11,7 +11,8 @@ module.exports = function(grunt) {
                     includePaths: ['assets/stylesheets', require('node-bourbon').includePaths]
                 },
                 files: {
-                    'assets/css/bootstrap.css': 'assets/stylesheets/bootstrap.scss'
+                    'assets/css/bootstrap.css': 'assets/stylesheets/bootstrap.scss',
+                    'assets/css/font-awesome.css':'node_modules/font-awesome/scss/font-awesome.scss'
                 }
             }
         },
@@ -35,16 +36,27 @@ module.exports = function(grunt) {
                     livereload: true
                 }
             }
-        }
+        },
+        
+        concat: {
+          options: {
+            separator: ';',
+          },
+          dist: {
+            src: ['assets/css/font-awesome.css', 'assets/css/bootstrap.css'],
+            dest: 'assets/css/bootstrap.css',
+          },
+        },
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default task(s).
     grunt.registerTask('build', ['sass']);
-    grunt.registerTask('default', ['build', 'watch']);
+    grunt.registerTask('default', ['build', 'watch', 'concat']);
 
 };
