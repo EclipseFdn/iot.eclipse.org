afbb787b2f70237de20786801293dd72964be744
Benjamin Cabé <benjamin.cabe@eclipse-foundation.org>
2018-08-13 12:07:26 +0200

    update footer in public/

--- public/gruntfile.js
+++ public/gruntfile.js
@@ -1,62 +1,67 @@
 module.exports = function(grunt) {
-
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        sass: {
-            dist: {
-                options: {
-                    outputStyle: 'compressed',
-                    includePaths: ['assets/stylesheets', require('node-bourbon').includePaths]
-                },
-                files: {
-                    'assets/css/bootstrap.css': 'assets/stylesheets/bootstrap.scss',
-                    'assets/css/font-awesome.css':'node_modules/font-awesome/scss/font-awesome.scss'
-                }
-            }
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'node_modules/font-awesome/fonts',
+          src: ['**'],
+          dest: 'assets/fonts',
+          filter: 'isFile'
+        }]
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          outputStyle: 'compressed',
+          includePaths: ['assets/stylesheets', require('node-bourbon').includePaths]
         },
+        files: {
+          'assets/css/bootstrap.css': 'assets/stylesheets/bootstrap.scss',
+          'assets/css/font-awesome.css': 'node_modules/font-awesome/scss/font-awesome.scss'
+        }
+      }
+    },
+    watch: {
+      grunt: {
+        files: ['Gruntfile.js']
+      },
+      sass: {
+        files: 'assets/stylesheets/**/*.scss',
+        tasks: ['build'],
+        options: {
+          livereload: true
+        }
+      },
+      html: {
+        files: ['*.html', '**/*.html'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['assets/css/font-awesome.css', 'assets/css/bootstrap.css'],
+        dest: 'assets/css/bootstrap.css',
+      },
+    },
+  });
 
-        watch: {
-            grunt: {
-                files: ['Gruntfile.js']
-            },
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-            sass: {
-                files: 'assets/stylesheets/**/*.scss',
-                tasks: ['sass'],
-                options: {
-                    livereload: true
-                }
-            },
-
-            html: {
-                files: ['*.html', '**/*.html'],
-                options: {
-                    livereload: true
-                }
-            }
-        },
-        
-        concat: {
-          options: {
-            separator: ';',
-          },
-          dist: {
-            src: ['assets/css/font-awesome.css', 'assets/css/bootstrap.css'],
-            dest: 'assets/css/bootstrap.css',
-          },
-        },
-    });
-
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-
-    // Default task(s).
-    grunt.registerTask('build', ['sass']);
-    grunt.registerTask('default', ['build', 'watch', 'concat']);
-
+  // Default task(s).
+  grunt.registerTask('build', ['copy', 'sass', 'concat']);
+  grunt.registerTask('default', ['build', 'watch']);
 };
