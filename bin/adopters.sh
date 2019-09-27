a2da124d14ce0d81c81dbbc1a98d632bf455279a
Eric Poirier <eric.poirier@eclipse-foundation.org>
2019-09-27 08:55:35 -0400

    Add adopters shell script
    Signed-off-by: Eric Poirier <eric.poirier@eclipse-foundation.org>

---
+++ bin/adopters.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ===========================================================================
+# Copyright (c) 2019 Eclipse Foundation and others.
+# 
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+# 
+# Contributors:
+# Christopher Guindon (Eclipse Foundation)
+# 
+# SPDX-License-Identifier: EPL-2.0
+# ===========================================================================
+
+rm -f static/assets/js/eclipsefdn.adopters.js
+touch static/assets/js/eclipsefdn.adopters.js
+(echo -n "var json_adopters = '"; ./node_modules/yamljs/bin/yaml2json data/adopters.yml ; echo "';"; cat static/js/adopters.js ) > static/assets/js/eclipsefdn.adopters.js
