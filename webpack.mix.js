c35cda1c227003013d7aca4c89499ebc8c10296a
Zhou Fang <zhou.fang@eclipse-foundation.org>
2021-11-10 11:17:12 -0500

    Updated webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,23 +11,15 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 
+require('./node_modules/eclipsefdn-solstice-assets/webpack-solstice-assets.mix.js');
 let mix = require('laravel-mix');
-require('laravel-mix-transpile-node-modules')
-mix.transpileNodeModules(['eclipsefdn-solstice-assets']);
+mix.EclipseFdnSolsticeAssets();
 
 mix.setPublicPath('static/assets');
 mix.setResourceRoot('../');
 
 // CSS
 mix.less('./less/styles.less', 'static/assets/css/styles.min.css');
-
-mix.webpackConfig({
-  resolve: {
-    alias: {
-      jquery: 'jquery/src/jquery',
-    },
-  },
-});
 
 mix.js('js/main.js', 'assets/js/scripts.min.js');
 
